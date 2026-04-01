d858e0bfd9b69a8fc70bd30735225bf9576b6e3f
İbrahim Taha Pınar <tahapinar32@gmail.com>
2026-04-01 17:19:40 +0300

    Completed

---
+++ Text To Speech & Speech To Text/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Text_To_Speech___Speech_To_Text
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            mainText = new RichTextBox();
+            read_btn = new Button();
+            talk_btn = new Button();
+            SuspendLayout();
+            // 
+            // mainText
+            // 
+            mainText.BackColor = SystemColors.MenuText;
+            mainText.Font = new Font("Snap ITC", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            mainText.ForeColor = SystemColors.InactiveBorder;
+            mainText.Location = new Point(12, 21);
+            mainText.Name = "mainText";
+            mainText.Size = new Size(776, 288);
+            mainText.TabIndex = 0;
+            mainText.Text = "";
+            // 
+            // read_btn
+            // 
+            read_btn.BackColor = SystemColors.HighlightText;
+            read_btn.FlatStyle = FlatStyle.Flat;
+            read_btn.ForeColor = SystemColors.ActiveCaptionText;
+            read_btn.Location = new Point(194, 347);
+            read_btn.Name = "read_btn";
+            read_btn.Size = new Size(94, 29);
+            read_btn.TabIndex = 1;
+            read_btn.Text = "Read";
+            read_btn.UseVisualStyleBackColor = false;
+            read_btn.Click += read_btn_Click;
+            // 
+            // talk_btn
+            // 
+            talk_btn.FlatStyle = FlatStyle.Flat;
+            talk_btn.Location = new Point(486, 347);
+            talk_btn.Name = "talk_btn";
+            talk_btn.Size = new Size(94, 29);
+            talk_btn.TabIndex = 2;
+            talk_btn.Text = "Talk";
+            talk_btn.UseVisualStyleBackColor = true;
+            talk_btn.Click += talk_btn_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonHighlight;
+            ClientSize = new Size(800, 450);
+            Controls.Add(talk_btn);
+            Controls.Add(read_btn);
+            Controls.Add(mainText);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private RichTextBox mainText;
+        private Button read_btn;
+        private Button talk_btn;
+    }
+}
---
+++ Text To Speech & Speech To Text/Form1.cs
@@ -0,0 +1,113 @@
+﻿using NAudio.Wave;
+using System.Net.Http.Headers;
+using System.Speech.Synthesis;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Text_To_Speech___Speech_To_Text
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer reader = new SpeechSynthesizer();
+        WaveInEvent waveIn;
+        WaveFileWriter waveWriter;
+        string tempWavPath = Path.Combine(Path.GetTempPath(), "whisper_input.wav");
+        bool isRecording = false;
+        readonly string openAiKey = Environment.GetEnvironmentVariable("API_KEY");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void read_btn_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(mainText.Text))
+            {
+                reader.SpeakAsync(mainText.Text);
+            }
+            else
+            {
+                MessageBox.Show("Please enter a text!!");
+            }
+        }
+
+        private void talk_btn_Click(object sender, EventArgs e)
+        {
+            if (!isRecording)
+            {
+                isRecording = true;
+                talk_btn.Text = "⏹";
+
+                waveIn = new WaveInEvent();
+                waveIn.WaveFormat = new WaveFormat(16000, 1);
+                waveWriter = new WaveFileWriter(tempWavPath, waveIn.WaveFormat);
+
+                waveIn.DataAvailable += (s, a) =>
+                {
+                    waveWriter.Write(a.Buffer, 0, a.BytesRecorded);
+                };
+
+                waveIn.StartRecording();
+            }
+            else
+            {
+                isRecording = false;
+                talk_btn.Text = "🎤";
+
+                waveIn.StopRecording();
+                waveWriter.Dispose();
+                waveIn.Dispose();
+
+                talk_btn.Enabled = false;
+                _ = TranscribeWithWhisperAsync();
+            }
+        }
+
+        private async Task TranscribeWithWhisperAsync()
+        {
+            try
+            {
+                using var client = new HttpClient();
+                client.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("Bearer", openAiKey);
+
+                using var form = new MultipartFormDataContent();
+
+                var fileBytes = File.ReadAllBytes(tempWavPath);
+                var fileContent = new ByteArrayContent(fileBytes);
+                fileContent.Headers.ContentType = new MediaTypeHeaderValue("audio/wav");
+
+                form.Add(fileContent, "file", "audio.wav");
+                form.Add(new StringContent("whisper-1"), "model");
+
+                var response = await client.PostAsync(
+                    "https://api.openai.com/v1/audio/transcriptions", form);
+
+                var json = await response.Content.ReadAsStringAsync();
+
+                var start = json.IndexOf("\"text\":\"") + 8;
+                var end = json.IndexOf("\"", start);
+                var text = json.Substring(start, end - start);
+
+                mainText.Invoke(() =>
+                {
+                    mainText.Text += text + Environment.NewLine;
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+            finally
+            {
+                talk_btn.Invoke(() => talk_btn.Enabled = true);
+            }
+        }
+    }
+}
